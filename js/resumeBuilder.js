4f087b4a2d402f20b29db5a671f6f32104e13aea
Ali Naqvi <aliqasimnaqvi@gmail.com>
2015-10-11 00:04:08 +0100

    Add 'starter' sections for Work Experience, Projects and Education. Fix formatting issue with biopic. Add contacts for email, twitter and github.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,37 +1,80 @@
+//create formatted versions of all objects
 
+//bio section formatted objects
 var formattedName = HTMLheaderName.replace("%data%", "Ali Naqvi");
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+var formattedContact = HTMLcontactGeneric.replace("%data%", "aliqasimnaqvi@gmail.com").replace("%contact%", "Email:");
 var formattedEmail = HTMLemail.replace("%data%", "aliqasimnaqvi@gmail.com");
-var formattedPicture = HTMLbioPic.replace("%data%", "https://media.licdn.com/media/p/2/005/073/013/0bc428b.jpg");
+var formattedTwitter = HTMLtwitter.replace("%data%", "@Ali_Qas");
+var formattedGithub = HTMLgithub.replace("%data%", "https://github.com/AliQas");
+var formattedLocation = HTMLlocation.replace("%data%", "London, UK");
+var formattedBioPic = HTMLbioPic.replace("%data%", "images/biopic.jpg");
 var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", "Welcome to my resume page!");
-var skills = ["awesomeness ", "programming ", "digital marketing "];
-var formattedSkills = HTMLskills.replace("%data%", skills); 
-var formattedWorkTitle = HTMLworkTitle.replace("%data%", "Business Director");
-var formattedYearsWorked = HTMLworkDates.replace("%data%", "2 years");
-var formattedWorkCity = HTMLworkLocation.replace("%data%", "London");
+var skills = ["digital marketing", " web development", " business development"];
+var formattedSkills = HTMLskills.replace("%data%", skills);
 
+//work section formatted objects
+var formattedCurrentEmployer = HTMLworkEmployer.replace("%data%", "Neo@Ogilvy");
+var formattedCurrentWorkTitle = HTMLworkTitle.replace("%data%", "Business Director");
+
+
+
+//create bio object
 var bio = {
 	"name" : formattedName,
 	"role" : formattedRole,
-	"email" : formattedEmail,
-	"biopic" : formattedPicture,
+	"contacts" : {
+		"email" : formattedEmail,
+		"twitter" : formattedTwitter,
+		"github" : formattedGithub,
+		"location" : formattedLocation
+	},
+
+	"biopic" : formattedBioPic,
 	"welcomemsg" : formattedWelcomeMsg,
-	"skills" : formattedSkills,
+	"skills" : formattedSkills
 };
 
-
-$("#header").prepend(bio.biopic);
-$("#header").append(bio.name);
-$("#header").append(bio.role);
-$("#letsConnect").append(bio.email);
-$("#header").append(bio.welcomemsg);
-$("#main").append(bio.skills);
-
-bio.work = {
-	"position" : formattedWorkTitle,
-	"yearsworked": formattedYearsWorked,
-	"workcity": formattedWorkCity
+//create work object
+var work = {
+	"currentjob" : formattedCurrentWorkTitle,
+	"employer" : formattedCurrentEmployer,
+	"yearsworked" : 2,
+	"workcity" : "London, UK"
 }
 
+//header section
+$("#header").prepend(HTMLskillsStart);
+$("#header").prepend(bio.skills);
+$("#header").prepend(bio.welcomemsg);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(bio.biopic);
 
-$("#main").append(bio.work);
+
+//top contacts section
+$("#topContacts").append(bio.contacts.email);
+$("#topContacts").append(bio.contacts.twitter);
+$("#topContacts").append(bio.contacts.github);
+$("#topContacts").append(bio.contacts.location);
+
+
+//work experience section
+$("#workExperience").prepend(HTMLworkStart);
+$("#workExperience").append(work.currentjob);
+$("#workExperience").append(work.employer);
+
+
+
+//projects section
+$("#projects").prepend(HTMLprojectStart);
+
+//education section
+$("#education").prepend(HTMLschoolStart);
+
+
+
+
+
+
+
